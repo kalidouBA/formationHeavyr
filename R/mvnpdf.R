26f2854dc1aafdb9673872fccd632190fc37c5b8
Kalidou <kalidou.ia.mlds@gmail.com>
2022-06-08 16:16:24 +0200

    delete some text

--- R/mvnpdf.R
+++ R/mvnpdf.R
@@ -1,4 +1,4 @@
-#' Density of a multivariate normal distribution (+ du super text)
+#' Density of a multivariate normal distribution
 #'
 #' We want to compute the value of the density of a
 #' multivariate normal distribution on R p at n points.
